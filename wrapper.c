daae47ca9d51810ca2dabebe19833ca9ba78279c
tertius42 <louthomas316@gmail.com>
2017-10-04 01:01:14 -0400

    Bug fixes

--- wrapper.c
+++ wrapper.c
@@ -20,7 +20,7 @@ int main(int argc, char * argv[]) {
 		}
 		pclose(wget);
 
-		fprintf(stderr,"converting...");
+		fprintf(stderr,"converting...\n");
 		fb2rss = popen("./fb2rss posts.html -oposts.1.xml", "r");
 		if (!fb2rss) {
 			fprintf(stderr, "error: fb2rss isn't available\n");
@@ -43,7 +43,7 @@ int main(int argc, char * argv[]) {
 			diff = popen("mv posts.1.xml posts.xml","r");
 			pclose(diff);
 
-			fprintf(stderr,"commiting...");
+			fprintf(stderr,"commiting...\n");
 			git = popen("git commit -a -m \"auto-commit by wrapper\"", "r");
 			if (!git) {
 				fprintf(stderr, "error: git isn't available\n");
@@ -51,20 +51,20 @@ int main(int argc, char * argv[]) {
 			}
 			pclose(git);
 
-			fprintf(stderr,"pushing...");
+			fprintf(stderr,"pushing...\n");
 			git = popen("git push", "r");
 			if (!git) {
 				fprintf(stderr, "error: git isn't available\n");
 				return 0;
 			}
 			pclose(git);
-			fprintf(stderr,"done!");
+			fprintf(stderr,"done!\n");
 		}
 
 		unsigned long now = time(0);
 		strftime(s, 256, "%M", localtime(&now));
 		int minutes = atoi(s);
-		int wait_time = 30 - (minutes % 30) - 1;
+		int wait_time = 30 - (minutes % 30);
 		wait_time *= 60;
 		strftime(s, 256, "%S", localtime(&now));
 		wait_time -= atoi(s);
